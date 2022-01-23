451be067023567b996853777df28f34fefb6d19f
Heiko Schuett <heiko.schuett@nyu.edu>
2022-01-22 19:43:15 -0500

    added scripts to run model evaluations separately per file

---
+++ Model code/matlab wrapper/auto_test_single.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=48GB
+#SBATCH --job-name=fourinarow
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ik1125@nyu.edu
+#SBATCH --output=4inarow_%j.out
+
+direc=$SCRATCH/fourinarow/Model\ code/matlab\ wrapper/test/network_${SLURM_ARRAY_TASK_ID}.csv
+params_path=$SCRATCH/fourinarow/Model\ code/matlab\ wrapper/fits/out_params.csv
+codedirec=$SCRATCH/fourinarow/Model\ code/matlab\ wrapper
+out_path=$SCRATCH/fourinarow/Model\ code/matlab\ wrapper/fits/out${SLURM_ARRAY_TASK_ID}
+
+module purge
+module load matlab/2020b
+
+echo "addpath(genpath('${codedirec}')); parpool($SLURM_CPUS_PER_TASK); test_model_single('${direc}', '${params_path}', '${out_path}'); exit;" | matlab -nodisplay
+
+echo "Done"
---
+++ Model code/matlab wrapper/test_model_single.m
@@ -0,0 +1,26 @@
+function [moves,loglik] = test_model_single(in_path, params_path, out_path)
+
+% Load all the groups
+
+data = load_data_mat(in_path);
+
+% Load the parameters
+params = csvread(params_path);
+
+% evaluate model on testing data
+c = 5;
+Nevals = 200;
+
+[loglik, moves] = eval_model(data,params,c,Nevals);
+
+% reorder dimensions to be compatible to python neural networks:
+moves = reshape(permute(reshape(moves, 9, 4, []), [3, 2, 1]), [], 36);
+
+% Save outputs to csv
+moves_path = sprintf('%s_moves.csv', out_path);
+lltest_path = sprintf('%s_lltest.csv', out_path);
+
+csvwrite(moves_path,moves);
+csvwrite(lltest_path,loglik);
+
+end
