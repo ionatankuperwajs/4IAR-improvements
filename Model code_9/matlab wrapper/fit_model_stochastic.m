015bb579e1afe714a9e718ef039eaeef7b6351bf
ionatankuperwajs <ik1125@nyu.edu>
2022-10-03 18:24:54 -0700

    added new model variant

---
+++ Model code_9/matlab wrapper/fit_model_stochastic.m
@@ -0,0 +1,43 @@
+function [params,loglik] = fit_model_stochastic(data,settings)
+
+badsopts = bads('defaults');
+badsopts.UncertaintyHandling = 1;
+badsopts.NoiseFinalSamples = 0;
+badsopts.MaxFunEvals = 2000;
+
+if nargin < 2
+	settings.x0 = [2,0.02,0.2,0.05,1.2,0.8,0.2,0.2,0.2,0.2,1,0.4,3.5,10];
+	settings.ub = [10,1,1,1,4,10,10,10,10,10,10,10,10,100];
+	settings.lb = [0.1,0.001,0,0.01,0.25,0,0,0,0,0,0,0,0,0];
+	settings.pub = [10,1,0.5,0.5,2,5,5,5,5,5,5,5,5,50];
+	settings.plb = [0.1,0.001,0,0.02,0.5,0.1,0.01,0.01,0.01,0.01,0,0,0,5];
+	settings.c = 1;
+    settings.N = 10000;
+end
+
+
+
+x0 = settings.x0;
+lb = settings.lb;
+ub = settings.ub;
+plb = settings.plb;
+pub = settings.pub;
+c = settings.c;
+Ntrials = settings.N;
+L = zeros(Ntrials,10);
+
+% data
+
+% times = generate_times(mean(L,2),c);
+
+fun=@(x) mean(estimate_loglik_ibs_stochastic(data,x,c,Ntrials));
+
+params = bads(fun,x0,lb,ub,plb,pub,[],badsopts);
+
+params
+
+for i=1:10
+	loglik(i) = fun(params);
+end
+
+end
---
+++ Model code_9/matlab wrapper/pad_input.m
@@ -0,0 +1,25 @@
+function theta = pad_input(theta)
+%PAD_INPUT Add other fixed parameters for four-in-a-row model.
+
+g=sprintf('%f ', theta);
+fprintf('Theta = %s\n', g)
+
+thresh = theta(1);      % pruning threshold
+delta = theta(3);       % feature drop rate
+w_center = theta(6);    % center weight
+w_opening = [theta(7); theta(8); theta(9); theta(10)];
+w = [theta(11); theta(12); theta(13); 10000];
+w_pseudo3 = theta(14);
+w_defensive = theta(15);
+lambda = theta(4);      % lapse rate
+c_act = theta(5);
+gamma = theta(2);       % stopping probability
+theta=[10000;  thresh; gamma; lambda; 1; 1; w_center; w_opening; repmat(w,4,1); 0; w_pseudo3; c_act*repmat(w,4,1); 0; c_act*w_pseudo3; repmat(delta,18,1)];
+
+% Addition: add the defensive weight to the three-in-a-row weights
+theta(14) = theta(14)+w_defensive;
+theta(19) = theta(18)+w_defensive;
+theta(24) = theta(22)+w_defensive;
+theta(29) = theta(26)+w_defensive;
+
+end
