d31867c1ff29f9e3a0971857a5e9183b1829c474
Ionatan Kuperwajs <ik1125@log-3.hpc.nyu.edu>
2022-10-25 17:03:18 -0400

    cluster changes

--- Model code_9/matlab wrapper/pad_input.m
+++ Model code_9/matlab wrapper/pad_input.m
@@ -6,13 +6,14 @@ fprintf('Theta = %s\n', g)
 
 thresh = theta(1);      % pruning threshold
 delta = theta(3);       % feature drop rate
-w_center = theta(6);    % center weight
-w_opening = [theta(7); theta(8); theta(9); theta(10)];
-w = [theta(11); theta(12); theta(13); 10000];
-w_pseudo3 = theta(14);
-w_defensive = theta(15);
+w_center = theta(5);    % center weight
+w_opening = [theta(6); theta(7); theta(8); theta(9)];
+w = [theta(10); theta(11); theta(12); 10000];
+w_pseudo3 = theta(13);
+w_defensive = theta(14);
 lambda = theta(4);      % lapse rate
-c_act = theta(5);
+% c_act = theta(5);
+c_act = 0.43389;
 gamma = theta(2);       % stopping probability
 theta=[10000;  thresh; gamma; lambda; 1; 1; w_center; w_opening; repmat(w,4,1); 0; w_pseudo3; c_act*repmat(w,4,1); 0; c_act*w_pseudo3; repmat(delta,18,1)];
 
