892ef1a2956b6717ca4bb82666e9a6b3961a5ad8
ionatankuperwajs <ik1125@nyu.edu>
2022-10-26 14:47:57 -0400

    new centrality decay model

---
+++ Model code_10/matlab wrapper/auto_test_single.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=48GB
+#SBATCH --job-name=fourinarow
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ik1125@nyu.edu
+#SBATCH --output=4inarow_%a.out
+
+direc=$SCRATCH/fourinarow/Model\ code_8/matlab\ wrapper/test/network_${SLURM_ARRAY_TASK_ID}.csv
+params_path=$SCRATCH/fourinarow/Model\ code_8/matlab\ wrapper/fits/out_params.csv
+codedirec=$SCRATCH/fourinarow/Model\ code_8/matlab\ wrapper
+out_path=$SCRATCH/fourinarow/Model\ code_8/matlab\ wrapper/fits/out${SLURM_ARRAY_TASK_ID}
+
+module purge
+module load matlab/2020b
+export MATLABPATH=$HOME/matlab-output
+
+export MATLAB_PREFDIR=$TMPDIR/.matlab/R2020b/
+mkdir $TMPDIR/.matlab
+cp -r $HOME/.matlab/R2020b $TMPDIR/.matlab/R2020b
+mkdir $TMPDIR/.matlab/local_cluster_jobs
+mkdir $TMPDIR/.matlab/local_cluster_jobs/R2020b
+
+echo "addpath(genpath('${codedirec}')); parpool($SLURM_CPUS_PER_TASK);test_model_single('${direc}', '${params_path}', '${out_path}'); exit;" | matlab -nodisplay
+
+echo "Done"
--- Model code_9/matlab wrapper/pad_input.m
+++ Model code_9/matlab wrapper/pad_input.m
@@ -7,12 +7,12 @@ fprintf('Theta = %s\n', g)
 thresh = theta(1);      % pruning threshold
 delta = theta(3);       % feature drop rate
 w_center = theta(6);    % center weight
-w_opening = [theta(7); theta(8); theta(9); theta(10)];
-w = [theta(11); theta(12); theta(13); 10000];
-w_pseudo3 = theta(14);
-w_defensive = theta(15);
+w_opening = [theta(7); theta(8); theta(9); theta(10)];  % corner opening weights
+w = [theta(11); theta(12); theta(13); 10000];           % feature weights, 4-in-a-row fixed
+w_pseudo3 = theta(14);      % 3-in-a-row feature weight with no space for 4, at the ends of the board
+w_defensive = theta(15);    % weight for defending against immediate losses
 lambda = theta(4);      % lapse rate
-c_act = theta(5);
+c_act = theta(5);       % active-passive scaling
 gamma = theta(2);       % stopping probability
 theta=[10000;  thresh; gamma; lambda; 1; 1; w_center; w_opening; repmat(w,4,1); 0; w_pseudo3; c_act*repmat(w,4,1); 0; c_act*w_pseudo3; repmat(delta,18,1)];
 
