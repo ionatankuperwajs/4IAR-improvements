fed869cd2ac99e46f6da398348c6dfca3c642957
Ionatan Kuperwajs <ikuperwajs@gmail.com>
2023-03-22 16:40:36 -0400

    Delete Model code_3 directory

--- Model code_3/matlab wrapper/install_mex.m
+++
@@ -1,6 +0,0 @@
-cd ../CPP
-mex -R2018a -v -output estimate_loglik_mex CXXFLAGS="$CXXFLAGS -Wall -pthread -Wextra -std=c++11 -O3 -fexpensive-optimizations" estimate_loglik_mex.cpp heuristic.cpp bfs.cpp features.cpp data_struct.cpp;
-mex -R2018a -v -output get_move_hist CXXFLAGS="$CXXFLAGS -Wall -pthread -Wextra -std=c++11 -O3 -fexpensive-optimizations" get_move_hist.cpp heuristic.cpp bfs.cpp features.cpp data_struct.cpp;
-cd ../'matlab wrapper'/
-movefile ../CPP/estimate_loglik_mex.mex* .
-movefile ../CPP/get_move_hist.mex* .
